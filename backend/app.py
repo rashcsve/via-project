c7e3bd8da9ad8b0407cb988e3952219d2717b1dc
Svetlana Rashchupkina <svetlana.rashchupkina@gmail.com>
2021-01-04 17:02:37 +0100

    :sparkles: Add swagger docs

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,24 @@
-import os
-import json
+import os, json
+from glob import glob
 from flask import Flask, redirect, url_for, request, render_template, jsonify
 from pymongo import MongoClient
 from bson import ObjectId, json_util
 from flask_cors import CORS
 from datetime import datetime
+from flask_swagger_ui import get_swaggerui_blueprint
+from flask_restplus import Api, Resource
 from restaurants import getRestaurants
 
 flask = Flask(__name__)
+app = Api(
+    app=flask,
+    version="1.0",
+    title="Daily Menu VIA App",
+    description=
+    "A simple SPA application for showing lunch menus of selected restaurants built on REST API."
+)
 CORS(flask, resources={r"/*": {"origins": "http://localhost:3000"}})
 
-# To change accordingly
-# print(os.environ)
-# client = MongoClient("db", 27017)
-# db = client.appdb
 client = MongoClient(
     "mongodb+srv://user:useruser@via.eforv.mongodb.net/via?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
 )
@@ -32,41 +37,58 @@ class JSONEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, o)
 
 
-@flask.route("/api/restaurants", methods=["GET"])
-def restaurants():
-    if (restaurantsCollection.count() == 0):
-        data = getRestaurants(restaurantsCollection)
-    else:
-        data = [items for items in restaurantsCollection.find()]
-        firstObjDate = data[0]['date']
-        if (firstObjDate != CURRENT_DATE):
-            restaurantsCollection.drop()
+"""Define namespace"""
+restaurants_name_space = app.namespace(
+    "api", description='Get info about restaurants')
+
+
+@restaurants_name_space.route("/restaurants")  # Define the route
+class RestaurantsList(Resource):
+    @app.doc(responses={200: 'OK'}, description="Get list of all restaurants"
+             )  # Documentation of route
+    def get(self):  # GET method of REST
+        if (restaurantsCollection.count() == 0):
             data = getRestaurants(restaurantsCollection)
+        else:
+            data = [items for items in restaurantsCollection.find()]
+            firstObjDate = data[0]['date']
+            if (firstObjDate != CURRENT_DATE):
+                restaurantsCollection.drop()
+                data = getRestaurants(restaurantsCollection)
 
-    dbItems = db.restaurants.find()
-    dbRestaurants = [items for items in dbItems]
-    for rest in dbRestaurants:
-        print(rest['name'])
-        restDate = rest['date']
-        restDateConverted = datetime.strptime(restDate, DATE_FORMAT)
-        print(restDate)
-        if (restDate == CURRENT_DATE):
-            data.insert(0, rest)
-    return JSONEncoder().encode(data)
+        dbItems = db.restaurants.find()
+        dbRestaurants = [items for items in dbItems]
+        for rest in dbRestaurants:
+            print(rest['name'])
+            restDate = rest['date']
+            restDateConverted = datetime.strptime(restDate, DATE_FORMAT)
+            print(restDate)
+            if (restDate == CURRENT_DATE):
+                data.insert(0, rest)
+        return JSONEncoder().encode(data)
 
 
-@flask.route("/api/new", methods=["POST"])
-def new():
-    result = db.restaurants.insert_one(request.json["restaurant"])
-    return str(result.inserted_id)
+@restaurants_name_space.route("/new")  # Define the route
+class AddNew(Resource):
+    @app.doc(responses={
+        200: 'OK',
+        400: 'Invalid Argument'
+    },
+             description="Create new restaurant")
+    def post(self):
+        result = db.restaurants.insert_one(request.json["restaurant"])
+        return str(result.inserted_id)
 
 
-@flask.route("/api/id/<_id>", methods=["GET"])
-def getId(_id):
-    restaurant = db.restaurants.find_one({"_id": ObjectId(_id)})
-    if not restaurant:
-        restaurant = restaurantsCollection.find_one({"_id": ObjectId(_id)})
-    return json.dumps(restaurant, default=json_util.default)
+@restaurants_name_space.route("/id/<_id>")  # Define the route
+class Restaurant(Resource):
+    @app.doc(responses={200: 'OK'},
+             description="Get the restaurant")  # Documentation of route
+    def get(self, _id):  # GET method of REST
+        restaurant = db.restaurants.find_one({"_id": ObjectId(_id)})
+        if not restaurant:
+            restaurant = restaurantsCollection.find_one({"_id": ObjectId(_id)})
+        return json.dumps(restaurant, default=json_util.default)
 
 
 if __name__ == "__main__":
